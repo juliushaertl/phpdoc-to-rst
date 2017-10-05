832fbf468422eeee2448db7d67924a0c6f89ed7a
Julius Härtl <jus@bitgrid.net>
2017-10-06 01:08:58 +0200

    Tests: Add testing structure

---
+++ tests/InterfaceBuilderTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jus
+ * Date: 06.10.17
+ * Time: 00:55
+ */
+
+namespace JuliusHaertl\PHPDocToRst\Test;
+
+use JuliusHaertl\PHPDocToRst\Builder\InterfaceBuilder;
+use PHPUnit\Framework\TestCase;
+
+class InterfaceBuilderTest extends TestCase {
+
+    public function testFoobar() {
+        $this->assertEquals(1, 0);
+    }
+}
